3e1286abfa78ca0ea7d38263327735f526a607a1
CAP_DEV <127816113+capapiluk@users.noreply.github.com>
2026-02-03 20:20:35 +0700

    Fix generators and update structure

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,29 +1,41 @@
 Blockly.JavaScript['ph_read_value'] = function(block) {
+    Blockly.JavaScript.definitions_['include_ph_sensor'] = '#include <Arduino.h>';
+    
+    if (!Blockly.JavaScript.definitions_['var_ph_offset']) {
+        Blockly.JavaScript.definitions_['var_ph_offset'] = 'float phOffset = 0;';
+    }
+    
     var pin = Blockly.JavaScript.valueToCode(block, 'pin', Blockly.JavaScript.ORDER_ATOMIC);
     
-    var code = `(function() {
-        var adc = analogRead(${pin});
-        var voltage = (adc * 3.3) / 4095;
-        var phValue = 7 + (2.5 - voltage) / 0.18 + phOffset;
-        return Math.round(phValue * 100) / 100;
-    })()`; 
+    var code = `([]() {
+    int adc = analogRead(${pin});
+    float voltage = (adc * 3.3) / 4095.0;
+    float phValue = 7.0 + (2.5 - voltage) / 0.18 + phOffset;
+    return phValue;
+})()`; 
     
     return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
 };
 
 Blockly.JavaScript['ph_read_voltage'] = function(block) {
+    Blockly.JavaScript.definitions_['include_ph_sensor'] = '#include <Arduino.h>';
+    
     var pin = Blockly.JavaScript.valueToCode(block, 'pin', Blockly.JavaScript.ORDER_ATOMIC);
     
-    var code = `(function() {
-        var adc = analogRead(${pin});
-        var voltage = (adc * 3.3) / 4095;
-        return Math.round(voltage * 1000) / 1000;
-    })()`;
+    var code = `([]() {
+    int adc = analogRead(${pin});
+    float voltage = (adc * 3.3) / 4095.0;
+    return voltage;
+})()`;
     
     return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
 };
 
 Blockly.JavaScript['ph_set_offset'] = function(block) {
+    if (!Blockly.JavaScript.definitions_['var_ph_offset']) {
+        Blockly.JavaScript.definitions_['var_ph_offset'] = 'float phOffset = 0;';
+    }
+    
     var offset = Blockly.JavaScript.valueToCode(block, 'offset', Blockly.JavaScript.ORDER_ATOMIC);
     
     var code = `phOffset = ${offset};\n`;
